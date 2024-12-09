66fd34cc3a6d937f2be8b05526dc443c64d2fd4d
Jonathan Ellis <jbellis@datastax.com>
2024-12-09 11:53:30 -0600

    more recursion

--- llmap.py
+++ llmap.py
@@ -4,6 +4,49 @@ import sys
 from pathlib import Path
 from tree_sitter_languages import get_language, get_parser
 
+def _process_class(child, query, indent):
+    """Process a class definition node"""
+    parts = []
+    class_body = None
+    for n, t in query.captures(child):
+        if t == 'name.definition.class':
+            parts.append('class ' + n.text.decode('utf8').strip())
+            continue
+        if t in ('class.modifiers', 'class.superclass', 'class.interfaces'):
+            if n.text:
+                parts.append(t + '->' + n.text.decode('utf8').strip())
+            continue
+        if t == 'class.body':
+            class_body = n
+    print(parts)
+    return ' '.join(parts), class_body
+
+def _process_interface(child, query, indent):
+    """Process an interface definition node"""
+    parts = []
+    interface_body = None
+    for n, t in query.captures(child):
+        if t in ('interface.modifiers', 'name.definition.interface', 'interface.extends'):
+            if n.text:
+                parts.append(n.text.decode('utf8').strip())
+        if t == 'interface.body':
+            interface_body = n
+    
+    return ' '.join(parts), interface_body
+
+def _process_method(child, query, indent):
+    """Process a method definition node"""
+    parts = []
+    for n, t in query.captures(child):
+        if t in ('method.modifiers', 'method.type', 'name.definition.method', 'method.params'):
+            if n.text:
+                parts.append(n.text.decode('utf8').strip())
+    return ' '.join(parts) + ' {...}'
+
+def _process_field(child, indent):
+    """Process a field definition node"""
+    return child.text.decode('utf8')
+
 def _process_node(node, query, indent_level=0):
     """Process a node recursively and return its skeleton parts"""
     skeleton = []
@@ -11,50 +54,22 @@ def _process_node(node, query, indent_level=0):
 
     for child, tag in query.captures(node):
         if tag == 'definition.class':
-            # Find all parts of the class declaration
-            parts = []
-            class_body = None
-            for n, t in query.captures(child):
-                if t == 'name.definition.class':
-                    parts.append('class ' + n.text.decode('utf8').strip())
-                    continue
-                if t in ('class.modifiers', 'class.superclass', 'class.interfaces'):
-                    if n.text:
-                        parts.append(n.text.decode('utf8').strip())
-                if t == 'class.body':
-                    class_body = n
-            
-            skeleton.append(indent + ' '.join(parts))
-            # Recursively process the class body
+            signature, class_body = _process_class(child, query, indent)
+            skeleton.append(indent + signature)
             if class_body:
                 skeleton.extend(_process_node(class_body, query, indent_level + 1))
             
         elif tag == 'definition.interface':
-            # Find all parts of the interface declaration
-            parts = []
-            class_body = None
-            for n, t in query.captures(child):
-                if t in ('interface.modifiers', 'name.definition.interface', 'interface.extends'):
-                    if n.text:
-                        parts.append(n.text.decode('utf8').strip())
-                if t == 'interface.body':
-                    class_body = n
-
-            skeleton.append(indent + ' '.join(parts))
-            # Recursively process the interface body
-            skeleton.extend(_process_node(class_body, query, indent_level + 1))
+            signature, interface_body = _process_interface(child, query, indent)
+            skeleton.append(indent + signature)
+            skeleton.extend(_process_node(interface_body, query, indent_level + 1))
             
         elif tag == 'definition.method':
-            # Find all parts of the method declaration
-            parts = []
-            for n, t in query.captures(child):
-                if t in ('method.modifiers', 'method.type', 'name.definition.method', 'method.params'):
-                    if n.text:
-                        parts.append(n.text.decode('utf8').strip())
-            skeleton.append(indent + ' '.join(parts) + ' {...}')
+            signature = _process_method(child, query, indent)
+            skeleton.append(indent + signature)
             
         elif tag == 'definition.field':
-            text = child.text.decode('utf8')
+            text = _process_field(child, indent)
             skeleton.append(indent + text)
 
     return skeleton
